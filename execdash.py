6a4bd599ce848d2b33823c24f302305db5ce0d16
Lin Wang <alina.wanglin428@gmail.com>
2024-03-01 14:21:30 +0100

    Update execdash.py

--- execdash.py
+++ execdash.py
@@ -1,11 +1,6 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from PIL import Image
-import plost
 import altair as alt
-import plotly.express as px
 
 # Page configuration
 st.set_page_config(
