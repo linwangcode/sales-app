6a1c20cd2f69c09aec976d1295dcd68e1c1bd43f
Lin Wang <alina.wanglin428@gmail.com>
2024-03-01 22:18:28 +0100

    Update execdash.py

--- execdash.py
+++ execdash.py
@@ -12,7 +12,7 @@ st.set_page_config(
 navigation_items = {
     "Homepage": "https://sales-app-homepage1.streamlit.app/",
     "Exec Dash": "none",
-    "Products Dash": "https://sales-app-itemsdash1.streamlit.app/"
+    "Products Dash": "https://sales-app-itemsdash1.streamlit.app/",
     "Customers Dash": "https://nothwindtraderscustomer.streamlit.app/~/+/"
 }
 
